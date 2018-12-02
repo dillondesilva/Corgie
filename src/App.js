a9a0f539af4608ca6eae9ee4a9f1dc67794c0d87
Dillon de Silva <dillon.desilva@geeksdiary.com>
2018-12-02 18:39:57 +1100

    Initialized Firebase

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
 
+import firebase from 'firebase';
+
 // Importing Components
 import Home from './Components/Home';
 import Main from './Components/Main';
 
 class App extends Component {
+  componentDidMount() {
+    var config = {
+      apiKey: "AIzaSyB1cL7czBZiXL9dvvubbIAqu3j_eJFrkus",
+      authDomain: "corgie-10f43.firebaseapp.com",
+      databaseURL: "https://corgie-10f43.firebaseio.com",
+      projectId: "corgie-10f43",
+      storageBucket: "corgie-10f43.appspot.com",
+      messagingSenderId: "428097027985"
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <div>
