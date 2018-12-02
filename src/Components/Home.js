dfa4d1d920b750ba6ce91698d24fefbc93990e33
Dillon de Silva <dillon.desilva@geeksdiary.com>
2018-12-02 13:28:21 +1100

    Created homepage component. Downloaded react-router-dom npm package

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+// This is the homepage component
+import React from 'react';
+
+class Home extends React.Component {
+  render () {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default Home;
