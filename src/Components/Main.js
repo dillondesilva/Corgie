4132b053720b382986527bd937305c95b2544da5
Dillon de Silva <dillon.desilva@geeksdiary.com>
2018-12-02 18:24:05 +1100

    Added a stub for a newBook() function in Main.js

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 class Main extends React.Component {
+  newBook () {
+    // This is a stub. newBook() will 
+    // allow the user to create a new set/group 
+    // of notes. Eg: A new science book
+  }
+
   render () {
     return (
       <div>
         <h1>Dashboard</h1>
-        <button>New Book +</button>
+        <button onClick={() => this.newBook()}>New Book +</button>
       </div>
     )
   }
